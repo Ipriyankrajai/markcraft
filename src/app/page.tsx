9d7fb07252d4e604bb5ce16597e0dbc4be964efd
Priyank Rajai <priyank73@hotmail.com>
2024-08-15 21:06:25 +0530

    feat: adding editor to page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+"use client";
+import Editor from "@/components/editor/advanced-editor";
+import { defaultValue } from "@/components/editor/default-values";
+import { ThemeToggle } from "@/components/theme-toggle";
+import { JSONContent } from "novel";
+import { useState } from "react";
+
 export default function Home() {
-  return <main>Hello from MarkCraft</main>;
+  const [value, setValue] = useState<JSONContent>(defaultValue);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col p-6 border max-w-xl w-full gap-6 rounded-md bg-card">
+        <div className="flex justify-between">
+          <h1 className="text-4xl font-semibold">MarkCraft</h1>
+          <ThemeToggle />
+        </div>
+        <Editor initialValue={value} onChange={setValue} />
+      </div>
+    </main>
+  );
 }
