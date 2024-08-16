9273e76239d36937456f58ce1f0785d95c538344
Priyank Rajai <priyank73@hotmail.com>
2024-08-16 20:05:55 +0530

    refactor: css for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { ThemeToggle } from "@/components/theme-toggle";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col p-6 border max-w-xl w-full gap-6 rounded-md bg-card">
+    <main className="flex min-h-screen flex-col items-center justify-between p-5">
+      <div className="flex flex-col p-6 border max-w-7xl w-full gap-6 rounded-md bg-card">
         <div className="flex justify-between">
           <h1 className="text-4xl font-semibold">MarkCraft</h1>
           <ThemeToggle />
